af8800d54e86c9b738e07ba946eb365f8e17011d
Ivan De Leon <ivandeleon_2301@hotmail.com>
2022-10-16 01:41:44 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - Base Apparel coming soon page solution
+
+This is a solution to the [Base Apparel coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Screenshot
+
+![](/design/desktop-preview.jpg)
+
+Link: https://remarkable-croquembouche-390aee.netlify.app/
+
+### Built with:
+
+- Parcel
+- SASS
+- Normalize
+
+🔧 Comando para instalar las dependencias 
+
+```
+npm install
+```
+🔧 Comando para iniciar el servidor de desarrollo
+```
+npm run dev
+```
+🔧 Comando para crear la build
+```
+npm run build
+```
---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const form        = document.querySelector(".main-content-wrapper__form");
+const email_input = document.querySelector(".main-content-wrapper__form-email-input");
+const error       = document.querySelector(".main-content-wrapper__form-error");
+const error_msg   = document.querySelector(".main-content-wrapper__form-error-message");
+
+// Esta funcion se encarga de varias cosas:
+/*
+1- Que el formulario no recargue el navegador.
+2- Tiene asignado el valor del input (inputValue) para compararlo con el regex de la funcion isEmail().
+3- Cambia la opacidad de los errores en 1 y el color del borde del input si el email puesto NO coincide con el regex, es decir NO es valido
+4- Oculta los mensajes de error y devuelve el color original al border del input en caso de que el email puesto SEA valido. (Si antes fue mal puesto, y luego bien se oculta)
+ */
+const validateEmail = (e) => {
+  e.preventDefault();
+  const inputValue = email_input.value.trim();
+
+  if (!isEmail(inputValue)) {
+    email_input.style.border = "1px solid hsl(0, 93%, 68%)";
+    error.style.opacity = 1;
+    error_msg.style.opacity = 1;
+  } else {
+    error.style.opacity = 0;
+    error_msg.style.opacity = 0;
+    email_input.style.border = "1px solid hsl(0, 36%, 70%)";
+  }
+};
+
+// La funcion que se encarga de comparar lo que esta puesto en el input con la expresion regular (regex).
+const isEmail = (email_input) => {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email_input);
+};
+
+// El EventListener que ejecutara la funcion validateEmail cuando se le haga click al boton.
+form.addEventListener("submit", validateEmail);
